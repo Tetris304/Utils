ab35944b33cb7baf5ba9292437cc9a45cd4f5edf
Tetris <55093170+Tetris304@users.noreply.github.com>
2020-04-20 09:28:42 +0800

    'win10激活位置移动到文档'

--- eslintRules.js
+++ eslintRules.js
@@ -985,12 +985,3 @@ module.exports = {
         ]
     }
 }
-
-// win10企业版LTSC激活方式-亲测有效 (2020-02-21 16:52:03)转载▼
-// C:\Windows\system32>slmgr -ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
-
-// C:\Windows\system32>slmgr -skms kms.03k.org
-
-// C:\Windows\system32>slmgr -ato
-
-// C:\Windows\system32>slmgr -dlv
